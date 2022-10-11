7a4e3bf73d4275db9f059add512dd1c11c8d9168
Oleh.Scherbak <oleh.scherbak@ukr.net>
2022-10-11 21:53:05 +0300

    hw#6.8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector('.login-form');
+const inputs = document.querySelectorAll('input');
 
+form.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+    event.preventDefault();
+    for (let inputIndex = 0; inputIndex < inputs.length; inputIndex += 1) {
+        if (inputs[inputIndex].value === "") {
+            alert("All fields must be filled!")
+            return;
+        }
+    }
+    console.log('hi!')
+    event.currentTarget.reset();
+}
