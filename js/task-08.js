9ad4a01bf93d9e031c8189828e1117879fcdf86c
Oleh.Scherbak <oleh.scherbak@ukr.net>
2022-10-13 22:42:30 +0300

    hw6.8-9

--- js/task-08.js
+++ js/task-08.js
@@ -4,12 +4,14 @@ const inputs = document.querySelectorAll('input');
 form.addEventListener("submit", handleSubmit);
 function handleSubmit(event) {
     event.preventDefault();
+    const formData ={ };
     for (let inputIndex = 0; inputIndex < inputs.length; inputIndex += 1) {
         if (inputs[inputIndex].value === "") {
             alert("All fields must be filled!")
             return;
         }
+        formData[inputs[inputIndex].name] = inputs[inputIndex].value;
     }
-    console.log('hi!')
+    console.log(formData);
     event.currentTarget.reset();
 }
